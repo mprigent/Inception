310849bd88dc688a2eb3955a06e4a695fc283912
mprigent <77842628+mprigent@users.noreply.github.com>
2022-09-28 16:56:18 +0200

    Update auto_config.sh

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,8 +1,10 @@
 #!bin/bash
+sleep 10
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
     wp config create	--allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD \
     					--dbhost=mariadb:3306 --path='/var/www/wordpress'
-
+					
+sleep 2
 wp core install     --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
 wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
 fi
